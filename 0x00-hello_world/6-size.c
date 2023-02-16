810125610a7ecfadcc72d691147ff32489eca120
SaneleSiso <sanelesiso@gmail.com>
2023-02-16 14:54:09 -0800

    improves betty coding style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,8 @@
 
 /**
  * main - Entry point
- * Description -Prints the size of various types depending on the computer it is compiled and run on.
+ * Description -Prints the size of various types depending
+ * on the computer it is compiled and run on.
  * Return: Always 0.
  */
 int main(void)
