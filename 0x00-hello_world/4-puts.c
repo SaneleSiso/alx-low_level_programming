ca7c3eb2f3b18b476c700ee56764eebf515e8a71
SaneleSiso <sanelesiso@gmail.com>
2023-02-16 14:05:21 -0800

    adds betty style to 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/*
+ * main - the entry point to the c program.
+ * return(0) - when main returns, the program stops
+ */
 
-int main(void)
+int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	iputs("\"Programming is like building a multilingual puzzle\n");
 	return(0);
 }
