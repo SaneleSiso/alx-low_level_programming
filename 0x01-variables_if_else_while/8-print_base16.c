c1955324a3f089bed8cbd70896ab9f8d7d6e5106
SaneleSiso <sanelesiso@gmail.com>
2023-02-17 16:18:34 -0800

    adds 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	char alpha;
+
+	for (x = 0; x <= 9; x++)
+		putchar('0' + x);
+	for (a = 'alpha'; alpha <= 'f'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
+
