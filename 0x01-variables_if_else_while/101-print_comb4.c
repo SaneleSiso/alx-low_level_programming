ddf83f116796813b76603e5271530f28980dc2d6
SaneleSiso <sanelesiso@gmail.com>
2023-02-20 05:24:54 -0800

    corrects the output of 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,16 +9,16 @@ int main(void)
 	
 	for (i = '0'; i < '9'; i++)
 	{
-		for (j = i + 1; j < '9'; j++)
+		for (j = i + 1; j <= '9'; j++)
 		{
-			for (k = j + 1; k < '9'; k++)
+			for (k = j + 1; k <= '9'; k++)
 			{
 				if ((i != j) != k)
 				{
 					putchar(i);
 					putchar(j);
 					putchar(k);
-					if (i == '8' && j == '9')
+					if (i == '7' && j == '8')
 						continue;
 					putchar(',');
 					putchar(' ');
