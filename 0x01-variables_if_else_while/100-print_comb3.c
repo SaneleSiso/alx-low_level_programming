81c4df24513ffaf578f47d24a08a03810604cb61
SaneleSiso <sanelesiso@gmail.com>
2023-02-20 00:22:30 -0800

    improves 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,22 +7,21 @@ int main(void)
 {
 	int first, last;
 
-	for (first = 0; first < 10; first++)
+	for (first = '0'; first < '9'; first++)
 	{
-		for (last = 0; last < 10; last++)
+		for (last = first+ 1; last <= '9'; last++)
 		{
-			putchar((first % 10) + '0');
-			putchar((last % 10) + '0');
-
-			if (first == 9 && last == 9)
+			if (first != last)
+			{
+				putchar((first);
+				putchar(last);
+				if (first == '8' && last == 9)
 				continue;
-
 			putchar(',');
 			putchar(' ');
+			}
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
