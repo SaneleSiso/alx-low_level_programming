5e346436c6cfca6204c8573d8d61f5f1ad16e2f9
SaneleSiso <sanelesiso@gmail.com>
2023-02-20 09:06:33 -0800

    adds 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int first, last;
+
+	for (first = '0'; first < '9'; first++)
+	{
+		for (last = first + 1; last <= '9'; last++)
+		{
+			if (first != last)
+			{
+				putchar(first);
+				putchar(last);
+				if (first == '8' && last == '9')
+				continue;
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
