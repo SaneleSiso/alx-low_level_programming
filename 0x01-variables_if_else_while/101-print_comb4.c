79175a015c37ea537b35df7a3c4ae00800857145
SaneleSiso <sanelesiso@gmail.com>
2023-02-20 05:42:37 -0800

    fixes betty coding style

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 	int i, j, k;
-	
+
 	for (i = '0'; i < '9'; i++)
 	{
 		for (j = i + 1; j <= '9'; j++)
@@ -27,5 +27,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
