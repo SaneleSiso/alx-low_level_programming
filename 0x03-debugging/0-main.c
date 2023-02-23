e1ab94f640ebf12507685b8c1f67e3ce24b4999f
SaneleSiso <sanelesiso@gmail.com>
2023-02-23 11:40:28 -0800

    fixes main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - test function that prints if integer is positive or negative
+ * main - test function that prints whether integer is positive or negative
  * Return: 0
  */
 
