fd121ebda8f0e327dddeb0cec64b98ef11e0c5e4
SaneleSiso <sanelesiso@gmail.com>
2023-03-01 11:34:26 -0800

    corrects 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,22 +5,26 @@
  * @src: input value to be concatenated
  * @n: The number of bytes from src to be appended to dest.
  * Return: dest
+ *
  */
-char *_strncpy(char *dest, char *src, int n)
+
+char *_strncat(char *dest, char *src, int n)
 {
+	int i;
 	int j;
 
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
-		dest[j] = src[j];
-		j++;
+	dest[i] = src[j];
+	i++;
+	j++;
 	}
-	while (j < n)
-	{
-		dest[j] = '\0';
-		j++;
-	}
-
+	dest[i] = '\0';
 	return (dest);
 }
