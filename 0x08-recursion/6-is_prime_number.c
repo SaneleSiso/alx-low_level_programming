fa7f24a7ed3760a252cdefd60ea62c50825d3454
SaneleSiso <sanelesiso@gmail.com>
2023-03-08 12:23:47 -0800

    updates a file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,22 +1,67 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * is_prime_number - checks if a number is prime
- * @n: the number
- * @i: the number of times to iterate
- * Return: 1 if number is prime and 0 otherwise
+ * divisible - function declaration
+ * @my_num: number to test
+ * @divisor: the divisor
+ * Return: 0 or 1
  */
-int check_prime(int n, int i)
+
+int divisible(int my_num, int divisor);
+
+/**
+ * is_prime_number - function definition
+ * @n: number to test
+ * Return: 0 or 1
+ */
+
+int is_prime_number(int n);
+
+/**
+ * divisible - function definition
+ * @my_num: the number to test
+ * @divisor: the divisor to use
+ * Description: checks the divisibility of a number
+ * Return: 0 for divisible, 1 for not
+ */
+
+int divisible(int my_num, int divisor)
 {
-	if (n <= 1)
+	if (my_num % divisor == 0)
+	{
 		return (0);
-
-	if (n % i == 0 && i > 1)
-		return (0);
-
-	if ((n / i) < i)
+	}
+	else if (divisor == my_num / 2)
+	{
 		return (1);
-
-	return (check_prime(n, i + 1));
+	}
+	else
+	{
+		return (divisible(my_num, divisor + 1));
+	}
 }
+
+/**
+ * is_prime_number - function definition
+ * @n: the number to be tested
+ * Description: find a prime number
+ * Return: 0 for not prime, 1 for prime
+ */
+
+int is_prime_number(int n)
+{
+	int divisor = 2;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n >= 2 && n <= 3)
+	{
+		return (1);
+	}
+	else
+	{
+		return (divisible(n, divisor));
+	}
+} 
