60fd5e9d7649be7f49cee6fc8876a830c53d5455
SaneleSiso <sanelesiso@gmail.com>
2023-03-13 10:52:25 -0700

    adds multiple files

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - frees an array
+ * @grid the array to be freed
+ * @height: the height of the array
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid != NULL || height != 0)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+		free(grid);
+	}
+}
+
