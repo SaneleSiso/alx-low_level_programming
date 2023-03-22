78ece98520abf695600b98fc4e264490578acd64
SaneleSiso <sanelesiso@gmail.com>
2023-03-22 09:18:41 -0700

    adds the task files for 0x0F-function_pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - a function that searches for an integer
+ * @array: integer array.
+ * @size: the size of the array.
+ * @cmp: a pointer to the function to be used to 
+ * compare values.
+ *
+ * Return: index of the first eement for which the cmp
+ * function does not return 0. If no element matches,
+ * return -1. If size <= 0, return -1.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
+	}
+
+	return (-1);
+}
