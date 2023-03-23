14802dd7e26174fa2a4032b2662642172e7b65f8
SaneleSiso <sanelesiso@gmail.com>
2023-03-23 08:53:18 -0700

    creates files for the task C - Variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
 #include <stdarg.h>
+#include <stdio.h>
+
 /**
  * struct print - print type with corresponding print function
  * @t: print type
