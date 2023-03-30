7712cea31d1ce2d8c2d1ff7134f38f2499e57d50
SaneleSiso <sanelesiso@gmail.com>
2023-03-30 08:33:28 -0700

    creates the files for the the task 0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * mystartupfunction - a function that will run before the main function is executed
+ *
+ * Return: void
+ */
+void __attribute__ ((constructor)) mystartupfunction(void)
+{
+	/* Apply the constructor attribute to myStartupFun() so that it */
+	/*  is executed before main() */
+
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
