5f08a7b5d6ebbdcfffc8529577a97b7e1fb33824
SaneleSiso <sanelesiso@gmail.com>
2023-03-30 09:31:12 -0700

    updates 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
+void first(void) __attribute__ ((constructor));
+
 /**
- * mystartupfunction - a function that will run before the main function is executed
- *
- * Return: void
+ * first - prints a sentence before the main
+ * function is executed
  */
-
-void __attribute__ ((constructor)) mystartupfunction(void)
+void first(void)
 {
-	/* Apply the constructor attribute to myStartupFun() so that it */
-	/*  is executed before main() */
-
-	printf("You're beat! and yet, you must allow,\n"
-		"I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
