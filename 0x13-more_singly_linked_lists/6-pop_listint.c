4fa727242e59de23bfdbc75d03145aefa74dcf4b
SaneleSiso <sanelesiso@gmail.com>
2023-04-05 00:18:49 -0700

    fixes coding style

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,8 +1,7 @@
 #include "lists.h"
 
 /**
- * pop_listint - deletes the head node of
- * a linked list
+ * pop_listint - deletes the head node of a linked list
  * @head: head of a list.
  *
  * Return: head node's data.
@@ -12,8 +11,10 @@ int pop_listint(listint_t **head)
 {
 	listint_t *tmp;
 	int i = 0;
+
 	if (head == NULL || *head == NULL)
 		return (0);
+
 	tmp = *head;
 	i = tmp->n;
 	*head = (*head)->next;
