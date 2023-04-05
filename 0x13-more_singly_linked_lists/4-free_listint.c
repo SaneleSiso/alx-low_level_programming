ad0fda0f92f64561655d32bfa63b1dd1a3beb36a
SaneleSiso <sanelesiso@gmail.com>
2023-04-05 00:15:11 -0700

    updates 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,11 +1,12 @@
 #include "lists.h"
 
 /**
- * free_listint - program frees a linked list
+ * free_listint - frees a linked list
  * @head: head of a list.
  *
  * Return: nothing.
  */
+
 void free_listint(listint_t *head)
 {
 	listint_t *tmp;
