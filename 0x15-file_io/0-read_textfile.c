5170d21c1c8d2253023c27efb70342634f837d1d
SaneleSiso <sanelesiso@gmail.com>
2023-04-10 13:38:14 -0700

    creates files for 0x15 file io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * read_textfile - a function that reads a text file and prints it to the standard output
+ * @filename: the name of the file
+ * @letters: the  number of letters to be printed
+ *
+ * Return: number of letters read and printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int i;
+	ssize_t j, k;
+	char *tmp;
+	if (!filename)
+		return (0);
+	i = open(filename, O_RDONLY);
+	if (i == -1)
+		return (0);
+	tmp = malloc(sizeof(char) * (letters));
+	if (!tmp)
+		return (0);
+       j = read(i, tmp, letters);
+	k = write(STDOUT_FILENO, tmp, j);
+	close(i);
+	free(tmp);
+	return (k);
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile - a function that reads a
+ * text file and prints it to the POSIX standard output
+ * @filename: the name of the file
+ * @letters: the letters it should read and print
+ *
+ * Return: the number of letters it could read and print
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, count = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (count = 0; text_content[count];)
+		{
+			count++;
+		}
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+	close(o);
+
+	return (1);
+}
