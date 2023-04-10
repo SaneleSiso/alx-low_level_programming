59f40d540b82fd769fd9a45c778e9e13b5c5e9f2
SaneleSiso <sanelesiso@gmail.com>
2023-04-10 15:14:26 -0700

    updates a file

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,8 +11,6 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	int clear_bit(unsigned long int *n, unsigned int index)
-{
 	if (n == NULL || (index > (sizeof(unsigned long int) * 8) - 1))
 		return (-1);
 	*n &= ~(1 << index);
