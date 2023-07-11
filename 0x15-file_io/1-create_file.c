07f699719b6a410e7a741a1930193b9d05c99ef5
SanehSiso <sanelesiso@gmail.com>
2023-07-11 05:37:59 -0700

    updates 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,26 +5,35 @@
  * @filename:the name of the file.
  * @text_content: content written in the file.
  *
- * Return: 1 if it success. -1 if it fails.
+ * Return: 1 if it succeeds. -1 if it fails.
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, count = 0;
+	int i;
+	int j = 0;
+	int k;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	if (text_content != NULL)
-	{
-		for (count = 0; text_content[count];)
-		{
-			count++;
-		}
-	}
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, count);
-	if (o == -1 || w == -1)
+
+	i = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (i == -1)
 		return (-1);
-	close(o);
+
+	if (!text_content)
+		text_content = "";
+
+	for (j; text_content[j]; j++)
+		;
+
+	k = write(i, text_content, j);
+
+	if (k == -1)
+		return (-1);
+
+	close(i);
+
 	return (1);
 }
